933686d48ecea1f2ec5cfedeef6dcdaa8b418991
zccarver001-gif <zccarver001@converse.edu>
2025-11-17 14:14:37 -0500

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,17 @@
 var express = require('express');
 var router = express.Router();
+var bodyParser = require('body-parser');
+
+router.use(bodyParser.urlencoded({extended: true}));
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { formData: null});
+});
+
+/*Post home page. */
+router.post('/', function(req, res, next) {
+  res.render('index', { formData: req.body});
 });
 
 module.exports = router;
